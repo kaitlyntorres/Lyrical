cd802d044282fb35f9030ae51cdc8b7a61f8dc82
Kaitlyn Torres <kaitlyntorres@mail.adelphi.edu>
2022-02-08 19:34:23 -0500

    firebase stuff

--- app/src/main/java/com/example/lyrical/loginActivity.kt
+++ app/src/main/java/com/example/lyrical/loginActivity.kt
@@ -1,11 +1,67 @@
     package com.example.lyrical
 
+    import android.content.Intent
     import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
+    import com.google.firebase.auth.FirebaseAuth;
+    import com.google.firebase.auth.ktx.auth
+    import com.google.firebase.ktx.Firebase
+    import android.widget.Toast
+    import android.widget.Button
+    import android.widget.EditText
+    import android.widget.TextView
 
-    class loginActivity: AppCompatActivity()  {
+//A toast is a view containing a quick little message for the user
+
+
+
+    lateinit var email: EditText
+    lateinit var passwd: EditText
+    private lateinit var loginbutton: Button
+
+    lateinit var auth: FirebaseAuth
+    class loginActivity: AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.loginpage)
+            // reads in data from login xml by its id
+            email = findViewById(R.id.email)
+            passwd = findViewById(R.id.password)
+            loginbutton = findViewById(R.id.submitbutton)
+            // connection to firebase
+            auth = Firebase.auth
+            // on click goes to login function
+            loginbutton.setOnClickListener {
+                login()
+            }
+        }
+
+
+        private fun login() {
+            // converts user input to string
+            val email = email.text.toString()
+            val password = passwd.text.toString()
+            if (email.isBlank()) {
+                Toast.makeText(this, "Email is blank. Try again", Toast.LENGTH_SHORT).show()
+                return
+            } else if (password.isBlank()) {
+                Toast.makeText(this, "Password is  blank. Try again", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    // if login is successful, continues on to app
+                    val i = Intent(this, guestActivity::class.java)
+                    startActivity(i)
+                } else {
+                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+
+
+                }
+
+            }
+
         }
     }
--- app/src/main/java/com/example/lyrical/registerActivity.kt
+++ app/src/main/java/com/example/lyrical/registerActivity.kt
@@ -2,10 +2,75 @@ package com.example.lyrical
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class registerActivity: AppCompatActivity()  {
+//A toast is a view containing a quick little message for the user
+
+
+class registerActivity: AppCompatActivity() {
+    // intialize variables
+    private lateinit var email: EditText
+    private lateinit var passwd: EditText
+    private lateinit var confirmpasswd: EditText
+    private lateinit var btnRegister: Button
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+        // find variable by id from xml
+        email = findViewById(R.id.email)
+        passwd = findViewById(R.id.password)
+        confirmpasswd = findViewById(R.id.confirmpassword)
+
+        btnRegister = findViewById(R.id.sumbitregister)
+        // connect to firebase
+        auth = Firebase.auth
+        // on click call function
+        btnRegister.setOnClickListener {
+            registerUser()
+        }
+
     }
-}
+
+    private fun registerUser() {
+        // converts user input to string
+        val email = email.text.toString()
+        val password = passwd.text.toString()
+        val confirmpassword = confirmpasswd.text.toString()
+
+        if (email.isBlank()) {
+            Toast.makeText(this, "Email is blank. Try again", Toast.LENGTH_SHORT).show()
+            return
+        } else if (password.isBlank() || confirmpassword.isBlank()) {
+            Toast.makeText(this, "Password is  blank. Try again", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (password != confirmpassword) {
+            Toast.makeText(this, "Passwords don't match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                    // main menu
+                    finish()
+                } else {
+                    Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+
+
+    }
+
+
+
